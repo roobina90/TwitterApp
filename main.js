59bbd6ccebcc3a22a200e993de7ef2b0013042ee
roobina90 <roobina1990@gmail.com>
2016-10-14 22:34:14 +0200

    drag drop start

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
-
+/* ---> model */
 const Http = {
     get: (url) => new Task((rej, res) => $.getJSON(url).fail(rej).done(res))
 };
 
+
+
+const Photo = daggy.tagged('src', 'x', 'y');
+const newPhoto = (url) => Photo(url, 0, 0);
 
 const Url = String;
 
@@ -14,7 +18,31 @@ const makeUrl = function(t) {
 const baseUrl = "https://api.github.com/users/";
 
 const extractImage = R.prop("avatar_url");
-const githubSearch = R.compose(R.map(extractImage), Http.get, makeUrl);
+const githubSearch = R.compose(R.map(R.compose(newPhoto, extractImage)), Http.get, makeUrl);
+
+ /* model <---- */
+const preventDefault = e => e.preventDefault();
+
+var Collage = React.createClass({
+    displayName: "Collage",
+    getInitialState() { return { photos: []}},
+    updatePhotos(xs) { 
+        this.setState({photos: xs});
+    }, 
+
+    onDrop({dataTransfer: dt}) { 
+        const src = dt.getData("text");
+        this.updatePhotos(R.append(src, this.state.photos));
+    },
+    render() {
+        const images = (this.state.photos).map(function(source) {
+            return <img src={source} width="100"/>
+        });
+        return ( 
+            <div id="collage" onDrop={this.onDrop} onDragOver={preventDefault}>{images}</div>
+        );
+    }
+});
 
 var GitHub = React.createClass({
     displayName: "GitHub",
@@ -32,16 +60,21 @@ var GitHub = React.createClass({
         githubSearch(this.state.term).fork(this.props.showError, this.updateResult);
     },
 
+    onDragStart({dataTransfer: dt, currentTarget: t}) {
+        dt.setData('text', t.src);
+    },
 
     render() {
-        const profileImages = (this.state.result).map(function(source) {
-            return <img src={source}/>
+        const drag = this.onDragStart;
+        const profileImages = (this.state.result).map(function(photo) {
+            return <img src={photo.src} key={photo.src} draggable={true} onDragStart={drag} width="100"/>
         });
         return (
             <div id="gitHub">
                 <input onChange={this.termChanged}/>
                 <button onClick={this.searchClicked}>Search</button>
                 <div id="results">{profileImages}</div>
+                <Collage />
             </div>
         );
     }
